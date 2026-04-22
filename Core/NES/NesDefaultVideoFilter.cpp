6641333f5883e93aefbdb5fadeb32ef75bc5ec32
Fiskbit <2811896+Fiskbit@users.noreply.github.com>
2026-04-22 15:40:12 -0700

    NES: Adds RGB-style emphasis for RGB PPU types.

--- Core/NES/NesDefaultVideoFilter.cpp
+++ Core/NES/NesDefaultVideoFilter.cpp
@@ -42,38 +42,52 @@ NesDefaultVideoFilter::NesDefaultVideoFilter(Emulator* emu) : BaseVideoFilter(em
 	InitLookupTable();
 }
 
-void NesDefaultVideoFilter::GenerateFullColorPalette(uint32_t paletteBuffer[512])
+void NesDefaultVideoFilter::GenerateFullColorPalette(uint32_t paletteBuffer[512], PpuModel model)
 {
-	for(int i = 0; i < 64; i++) {
-		for(int j = 1; j < 8; j++) {
-			double redColor = (uint8_t)(paletteBuffer[i] >> 16);
-			double greenColor = (uint8_t)(paletteBuffer[i] >> 8);
-			double blueColor = (uint8_t)paletteBuffer[i];
-			if((i & 0x0F) <= 0x0D) {
-				//Emphasis doesn't affect columns $xE and $xF
-				if(j & 0x01) {
-					//Intensify red
-					greenColor *= 0.84;
-					blueColor *= 0.84;
+	if(model == PpuModel::Ppu2C02) {
+		for(int i = 0; i < 64; i++) {
+			for(int j = 1; j < 8; j++) {
+				double redColor = (uint8_t)(paletteBuffer[i] >> 16);
+				double greenColor = (uint8_t)(paletteBuffer[i] >> 8);
+				double blueColor = (uint8_t)paletteBuffer[i];
+				if((i & 0x0F) <= 0x0D) {
+					//Emphasis doesn't affect columns $xE and $xF
+					if(j & 0x01) {
+						//Intensify red
+						greenColor *= 0.84;
+						blueColor *= 0.84;
+					}
+					if(j & 0x02) {
+						//Intensify green
+						redColor *= 0.84;
+						blueColor *= 0.84;
+					}
+					if(j & 0x04) {
+						//Intensify blue
+						redColor *= 0.84;
+						greenColor *= 0.84;
+					}
 				}
-				if(j & 0x02) {
-					//Intensify green
-					redColor *= 0.84;
-					blueColor *= 0.84;
-				}
-				if(j & 0x04) {
-					//Intensify blue
-					redColor *= 0.84;
-					greenColor *= 0.84;
-				}
+
+				uint8_t r = (uint8_t)(redColor > 255 ? 255 : redColor);
+				uint8_t g = (uint8_t)(greenColor > 255 ? 255 : greenColor);
+				uint8_t b = (uint8_t)(blueColor > 255 ? 255 : blueColor);
+
+				uint32_t color = 0xFF000000 | (r << 16) | (g << 8) | b;
+				paletteBuffer[(j << 6) | i] = color;
 			}
-
-			uint8_t r = (uint8_t)(redColor > 255 ? 255 : redColor);
-			uint8_t g = (uint8_t)(greenColor > 255 ? 255 : greenColor);
-			uint8_t b = (uint8_t)(blueColor > 255 ? 255 : blueColor);
-
-			uint32_t color = 0xFF000000 | (r << 16) | (g << 8) | b;
-			paletteBuffer[(j << 6) | i] = color;
+		}
+	} else {
+		//RGB PPUs
+		for(int i = 0; i < 64; i++) {
+			for(int j = 1; j < 8; j++) {
+				//Max out the intensified channel.
+				uint32_t color = paletteBuffer[i];
+				if(j & 0x01) color |= 0xFF0000; //R
+				if(j & 0x02) color |= 0x00FF00; //G
+				if(j & 0x04) color |= 0x0000FF; //B
+				paletteBuffer[(j << 6) | i] = color;
+			}
 		}
 	}
 }
@@ -84,7 +98,7 @@ void NesDefaultVideoFilter::GetFullPalette(uint32_t palette[512], NesConfig& nes
 		memcpy(palette, nesCfg.UserPalette, sizeof(nesCfg.UserPalette));
 		if(!nesCfg.IsFullColorPalette) {
 			//Generate emphasis palette colors
-			GenerateFullColorPalette(palette);
+			GenerateFullColorPalette(palette, model);
 		}
 	} else {
 		if(nesCfg.UseCustomVsPalette) {
@@ -98,7 +112,7 @@ void NesDefaultVideoFilter::GetFullPalette(uint32_t palette[512], NesConfig& nes
 			}
 		} else {
 			memcpy(palette, _ppuPaletteArgb[(int)model], sizeof(_ppuPaletteArgb[(int)_ppuModel]));
-			GenerateFullColorPalette(palette);
+			GenerateFullColorPalette(palette, model);
 		}
 	}
 }
