6c438f9e84e8825719e76800b83faea8d2df4b27
Adrian Siekierka <kontakt@asie.pl>
2026-05-09 20:38:23 +0200

    WS: Initial WW flash chip emulation.

--- Core/WS/Carts/WsCartFlash.h
+++ Core/WS/Carts/WsCartFlash.h
@@ -5,8 +5,22 @@
 #include "Utilities/ISerializable.h"
 #include "Shared/MemoryType.h"
 
-class WsCartFlash : public WsCart
+class WsCartFlash final : public WsCart
 {
+private:
+	enum class Mode : uint8_t
+	{
+		Read = 0,
+		Autoselect,
+		Fast,
+		Program,
+		FastProgram,
+		Erase
+	};
+
+	Mode _mode[2];
+	uint8_t _unlock;
+
 public:
 	WsCartFlash();
 	virtual ~WsCartFlash() {}
@@ -15,4 +29,6 @@ public:
 
 	uint8_t ReadMemory(uint32_t addr) override;
 	void WriteMemory(uint32_t addr, uint8_t value) override;
+
+	void Serialize(Serializer& s) override;
 };
