f62dbc20f1e80bba94ed30da5a7c20f2dc3bfe64
HeeminTV <heeminwelcome1@gmail.com>
2026-05-10 13:03:34 +0900

    Implement N163 internal RAM auto-increasement address clipping

--- Core/NES/Mappers/Audio/Namco163Audio.h
+++ Core/NES/Mappers/Audio/Namco163Audio.h
@@ -168,7 +168,7 @@ public:
 			case 0x4800:
 				_console->GetEmulator()->ProcessMemoryAccess<CpuType::Nes, MemoryType::NesMapperRam, MemoryOperationType::Write>(_ramPosition + _ramOffset, value);
 				_internalRam[_ramPosition] = value;
-				if(_autoIncrement) {
+				if(_autoIncrement && _ramPosition != 0x7F) {
 					_ramPosition = (_ramPosition + 1) & 0x7F;
 				}
 				break;
@@ -189,7 +189,7 @@ public:
 			case 0x4800: {
 				value = _internalRam[_ramPosition];
 				_console->GetEmulator()->ProcessMemoryAccess<CpuType::Nes, MemoryType::NesMapperRam, MemoryOperationType::Read>(_ramPosition + _ramOffset, value);
-				if(_autoIncrement) {
+				if(_autoIncrement && _ramPosition != 0x7F) {
 					_ramPosition = (_ramPosition + 1) & 0x7F;
 				}
 				break;
