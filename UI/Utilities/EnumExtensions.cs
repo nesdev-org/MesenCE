6e23ca2e54cadcb4557f89184f65699c84f8615c
Adrian Siekierka <kontakt@asie.pl>
2026-05-10 12:39:40 +0200

    Code: Fix EnumExtensions.GetAttribute on .NET 10.

--- UI/Utilities/EnumExtensions.cs
+++ UI/Utilities/EnumExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -9,9 +10,10 @@ namespace Mesen.Utilities
 {
 	public static class EnumExtensions
 	{
+		[UnconditionalSuppressMessage("Trimming", "IL2075", Justification = "Fields are not trimmed for enum types")]
 		public static T? GetAttribute<T>(this Enum val) where T : Attribute
 		{
-			return val.GetType().GetMember(val.ToString())[0].GetCustomAttribute<T>();
+			return val.GetType().GetField(val.ToString(), BindingFlags.Public | BindingFlags.Static)!.GetCustomAttribute<T>();
 		}
 	}
 }
