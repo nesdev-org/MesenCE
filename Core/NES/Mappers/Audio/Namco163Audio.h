831e1a4482bca945f2e807a9df3beb0e23cddecb
HeeminTV <heeminwelcome1@gmail.com>
2026-05-13 01:19:04 +0900

    Update Namco163Audio.h

--- Core/NES/Mappers/Audio/Namco163Audio.h
+++ Core/NES/Mappers/Audio/Namco163Audio.h
@@ -169,7 +169,7 @@ public:
 				_console->GetEmulator()->ProcessMemoryAccess<CpuType::Nes, MemoryType::NesMapperRam, MemoryOperationType::Write>(_ramPosition + _ramOffset, value);
 				_internalRam[_ramPosition] = value;
 				if(_autoIncrement && _ramPosition != 0x7F) {
-					_ramPosition = (_ramPosition + 1) & 0x7F;
+					_ramPosition++;
 				}
 				break;
 			case 0xE000:
@@ -190,7 +190,7 @@ public:
 				value = _internalRam[_ramPosition];
 				_console->GetEmulator()->ProcessMemoryAccess<CpuType::Nes, MemoryType::NesMapperRam, MemoryOperationType::Read>(_ramPosition + _ramOffset, value);
 				if(_autoIncrement && _ramPosition != 0x7F) {
-					_ramPosition = (_ramPosition + 1) & 0x7F;
+					_ramPosition++;
 				}
 				break;
 			}
