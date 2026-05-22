f8b7178a0c5ef7b8cb310f414efa7603c3c10fc4
eclectic.sh <ec@eclectic.sh>
2026-05-22 13:55:07 -0600

    MacOS: Enable GameController background events

--- MacOS/MacOSKeyManager.mm
+++ MacOS/MacOSKeyManager.mm
@@ -43,6 +43,10 @@ MacOSKeyManager::MacOSKeyManager(Emulator* emu)
 
 	_disableAllKeys = false;
 
+	if(@available(macOS 11.3, *)) {
+		GCController.shouldMonitorBackgroundEvents = YES;
+	}
+
 	NSEventMask eventMask = NSEventMaskKeyDown | NSEventMaskKeyUp | NSEventMaskFlagsChanged;
 
 	_eventMonitor = [NSEvent addLocalMonitorForEventsMatchingMask:eventMask handler:^ NSEvent* (NSEvent* event) {
