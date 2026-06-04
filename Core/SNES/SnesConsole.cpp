d02285e72f6ffdae97d94d2f09f0c4ca644bbe7e
Sour <mesenemu@gmail.com>
2026-06-04 13:39:18 +0900

    Debugger: SNES - GSU - Allow labels on GSU work ram Also fixes GsuWorkRam not being able to lookup its relative CPU address (which caused the "CPU Addr" column to always read <unavailable> in the label list)

--- Core/SNES/SnesConsole.cpp
+++ Core/SNES/SnesConsole.cpp
@@ -490,6 +490,7 @@ AddressInfo SnesConsole::GetRelativeAddress(AddressInfo& absAddress, CpuType cpu
 		case MemoryType::SnesPrgRom:
 		case MemoryType::SnesWorkRam:
 		case MemoryType::SnesSaveRam:
+		case MemoryType::GsuWorkRam:
 		case MemoryType::SufamiTurboFirmware: {
 			if(!mappings) {
 				return unmapped;
