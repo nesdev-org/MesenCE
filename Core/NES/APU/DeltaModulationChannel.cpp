2137b301bbaf5c45ceffa38a68e12d16596fea4d
xtophyr <22761542+xtophyr@users.noreply.github.com>
2026-09-25 22:51:22 -0400

    Debugger: NES - Register Viewer - Added NES DMC Bits Remaining (#282)

--- Core/NES/APU/DeltaModulationChannel.cpp
+++ Core/NES/APU/DeltaModulationChannel.cpp
@@ -313,6 +313,7 @@ ApuDmcState DeltaModulationChannel::GetState()
 	state.SampleAddr = _sampleAddr;
 	state.NextSampleAddr = _currentAddr;
 	state.SampleLength = _sampleLength;
+	state.BitsRemaining = _bitsRemaining;
 	return state;
 }
 
